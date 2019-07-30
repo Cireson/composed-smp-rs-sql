23f20ea74608886a0679b14c3e7966eaf8daf5ce
jjburr <31635161+jjburr@users.noreply.github.com>
2019-07-31 10:40:49 +1200

    Update Install_Docker_And_Docker-Compose.ps1

--- Install_Docker_And_Docker-Compose.ps1
+++ Install_Docker_And_Docker-Compose.ps1
@@ -3,4 +3,5 @@ Install-Module DockerMsftProvider -Force
 Install-Package Docker -ProviderName DockerMsftProvider -Force
 
 #Docker Compose
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 Invoke-WebRequest -UseBasicParsing -Outfile $Env:ProgramFiles\docker\docker-compose.exe https://github.com/docker/compose/releases/download/1.24.0/docker-compose-Windows-x86_64.exe
